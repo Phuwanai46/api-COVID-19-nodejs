2351d3d4509297f74da34d48868583dcea7b43d3
Phuwanai46 <107763756+Phuwanai46@users.noreply.github.com>
2022-07-22 09:25:16 +0700

    Update covid.js

--- covid.js
+++ covid.js
@@ -1,7 +1,7 @@
 const request = require('request')
    
 // Request URL
-var url = 'https://lnwcodeapi.ml/latest';
+var url = 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-all';
   
 request(url, (error, response, body)=>{
      
@@ -10,6 +10,7 @@ request(url, (error, response, body)=>{
     
     // Printing status code
     console.log(response.statusCode);
+    console.log("ติดเชื้อใหม่วันนี้ :  " +user.new_case);
       
     // Printing body
     console.log(body);
